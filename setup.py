95bd30d8181614c1f4ed2cfe7109c9e5ef4cbe4c
Nicole Guobadia <nguob@uw.edu>
2023-10-30 15:37:07 -0700

    Refactoring

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 """Python `setup.py` for `rtk` package. Adapted from https://github.com/rochacbruno/python-project-template/blob/main/setup.py."""
+import os
 from setuptools import setup, find_packages
 from rtk import __version__
+
+loc = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_requirements():
@@ -14,12 +17,18 @@ def get_requirements():
 
 requirements = get_requirements()
 setup(
-    name="rtk",
     version=__version__,
     author="teddygu",
     description="General purpose toolkit for working with medical imaging data. Contains experiment management with Mlflow, Hydra, and AzureML.",
+    name="rtk",
+    entry_points={
+        "console_scripts": [
+            f"'rtk-job'=scripts.run_job:main",
+        ],
+    },
     long_description="",
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    scripts=["./scripts/run_job.py"]
     # requires=requirements,
 )
